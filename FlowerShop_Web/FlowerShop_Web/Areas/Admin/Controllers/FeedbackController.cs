4b43d6711fbe0d47d1b86fb4fd26478310c97a01
ThanhLeT0bito <ltthanh22112002@gmail.com>
2023-12-12 11:35:24 +0700

    update all v1

---
+++ FlowerShop_Web/FlowerShop_Web/Areas/Admin/Controllers/FeedbackController.cs
@@ -0,0 +1,40 @@
+﻿using Flower_Models;
+using Flower_Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerShop_Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FeedbackController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public FeedbackController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var listFeedback = _context.Feedbacks.ToList();
+            return View(listFeedback);
+        }
+        public IActionResult Delete(int? id)
+        {
+            var feedback = _context.Feedbacks.FirstOrDefault(x => x.FeedbackId == id);  
+            if (feedback != null)
+            {
+                _context.Feedbacks.Remove(feedback);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
